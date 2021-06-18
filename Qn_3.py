1ede0cbffadde40fcadb7273688053dfb17cf29a
Aakash V <58223250+aakashv129@users.noreply.github.com>
2021-06-18 18:25:42 +0530

    Create Qn_3.py

---
+++ Qn_3.py
@@ -0,0 +1,12 @@
+import time
+lower=int(input("Enter lower range: "))
+upper=int(input("Enter upper range: "))
+
+for i in range(lower,upper + 1):
+    if i>1:
+        for j in range(2,num):
+            if (i%j) == 0:
+                break
+        else:
+            print(i)
+            time.sleep(5)
